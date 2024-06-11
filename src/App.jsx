bdf6ffd7f75a95b9a47fc97c751fa018106d3413
JiDuy <jiduy02@gmail.com>
2024-06-11 15:36:44 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+      <div className="h-screen w-screen flex justify-center items-center">
+        <h1 className='text-red-600 text-9xl font-semibold'>
+          JIDUY
+        </h1>
+      </div>
+  )
+}
+
+export default App
